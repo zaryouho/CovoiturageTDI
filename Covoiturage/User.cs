a9560cc6754dbc0f0790a7fe2dae30f7f0b5b088
Hamza <black_sempston@outlook.fr>
2020-06-21 23:45:45 +0200

    jQueryAjax

---
+++ Covoiturage/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Covoiturage
+{
+    public class User
+    {
+        public string email { get; set; }
+        public bool isAvailable { get; set; }
+        
+    }
+}
---
+++ Covoiturage/emailAvailabilityWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="emailAvailabilityWebService.asmx.cs" Class="Covoiturage.emailAvailabilityWebService" %>
